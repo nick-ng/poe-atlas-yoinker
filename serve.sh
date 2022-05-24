7d064885c6a28f986d6fad10310c90e722d368b3
Nick Ng <nicholas.cj.ng@gmail.com>
2022-05-24 18:44:06 +1200

    make serve.sh simpler

--- serve.sh
+++ serve.sh
@@ -2,10 +2,4 @@
 
 cd /home/pi/git/poe-atlas-yoinker
 
-/usr/bin/git checkout main
-
-/usr/bin/git pull
-
-/home/pi/.nvm/versions/node/v16.15.0/bin/npm ci
-
 /home/pi/.nvm/versions/node/v16.15.0/bin/node server.js
